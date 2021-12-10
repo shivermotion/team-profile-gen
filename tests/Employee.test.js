bc8167142becbd45b41b081c01972c9643e2ce73
Jason Day <jason.day.blue@gmail.com>
2021-12-09 23:50:15 -0600

    building test code

---
+++ tests/Employee.test.js
@@ -0,0 +1,15 @@
+describe("Employee", () => {
+	describe("instantiation", () => {
+		//Arrange
+		//Act
+		//Assert
+	});
+
+	describe("getEmail", () => {});
+
+	describe("getId", () => {});
+
+	describe("getName", () => {});
+
+	describe("getRole", () => {});
+});
