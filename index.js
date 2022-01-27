a10bfd1e997f63ab499384d3b049428088fb32ba
Jason Day <jason.day.blue@gmail.com>
2022-01-27 01:32:17 -0600

    save

--- index.js
+++ index.js
@@ -145,7 +145,21 @@ const generateHTML = ({ name, email, id, officeNumber, github, school }) =>
 	</head>
 	
 	<body>
-	
+	<div class="bd-example">
+        <div class="row  row-cols-1 row-cols-md-2 g-4">
+         
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
 	
 	
 	
