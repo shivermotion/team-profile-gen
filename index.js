42f0cc285770f9797065fab98c89b35a0e2112e0
Jason Day <jason.day.blue@gmail.com>
2022-01-27 02:01:26 -0600

    need to just generate the html by pluggin in team respones from team array

--- index.js
+++ index.js
@@ -130,10 +130,10 @@ async function combinePrompts() {
 
 combinePrompts();
 
-const generateHTML = ({ name, email, id, officeNumber, github, school }) =>
+const generateHTML = () =>
 	`<!doctype html>
 	<html lang="en">
-
+	
 	<head>
 		<!-- Required meta tags -->
 		<meta charset="utf-8">
@@ -145,26 +145,179 @@ const generateHTML = ({ name, email, id, officeNumber, github, school }) =>
 	</head>
 	
 	<body>
-	<div class="bd-example">
-        <div class="row  row-cols-1 row-cols-md-2 g-4">
-         
-                <div class="col-md-8">
-                  <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        </div>
+		<!--navbar-->
+		<nav class="navbar navbar-light bg-light">
+			<div class="container-fluid">
+				<a class="navbar-brand" href="#">
+					<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor"
+						class="bi bi-person-video" viewBox="0 0 16 16">
+						<path d="M8 9.05a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" />
+						<path
+							d="M2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H2Zm10.798 11c-.453-1.27-1.76-3-4.798-3-3.037 0-4.345 1.73-4.798 3H2a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1h-1.202Z" />
+					</svg>
+					Team Profile Generator
+				</a>
+			</div>
+		</nav>
+		<!--card grid-->
+		<div class="container">
+			<div class="row row-cols-2">
+				<!--Manager-->
+				<div class="col">
+					<div class="card">
+						<div class="row g-0">
+							<div class="col-md-4">
+								<image class="bd-placeholder-img" width="100%" height="250"
+									src="assets/man-avatar-pngrepo-com.png" role="img" aria-label="Placeholder: Image"
+									preserveAspectRatio="xMidYMid slice" focusable="false">
+									<title>Placeholder</title>
+									<rect width="100%" height="100%" fill="#868e96"></rect>
+								</image>
+	
+							</div>
+							<div class="col-md-8">
+								<div class="card-body">
+									<h5 class="card-title">Manager</h5>
+									<ul style="list-style: none;">
+										<li>
+											<p class="card-text">${manager.name}</p>
+										</li>
+										<li>
+											<p class="card-text">${email}</p>
+										</li>
+										<li>
+											<p class="card-text">${id}</p>
+										</li>
+										<li>
+											<p class="card-text">${officeNumber}</p>
+										</li>
+	
+									</ul>
+	
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!--Engineer-->
+				<div class="col">
+					<div class="card">
+						<div class="row g-0">
+							<div class="col-md-4">
+								<image class="bd-placeholder-img" width="100%" height="250"
+									src="assets/man-avatar-pngrepo-com.png" role="img" aria-label="Placeholder: Image"
+									preserveAspectRatio="xMidYMid slice" focusable="false">
+									<title>Placeholder</title>
+									<rect width="100%" height="100%" fill="#868e96"></rect>
+								</image>
+	
+							</div>
+							<div class="col-md-8">
+								<div class="card-body">
+									<h5 class="card-title">Engineer</h5>
+									<ul style="list-style: none;">
+										<li>
+											<p class="card-text">${name}</p>
+										</li>
+										<li>
+											<p class="card-text">${email}</p>
+										</li>
+										<li>
+											<p class="card-text">${id}</p>
+										</li>
+										<li>
+											<p class="card-text">${gitub}</p>
+										</li>
+	
+									</ul>
+	
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!--Intern-->
+				<div class="col">
+					<div class="card">
+						<div class="row g-0">
+							<div class="col-md-4">
+								<image class="bd-placeholder-img" width="100%" height="250"
+									src="assets/man-avatar-pngrepo-com.png" role="img" aria-label="Placeholder: Image"
+									preserveAspectRatio="xMidYMid slice" focusable="false">
+									<title>Placeholder</title>
+									<rect width="100%" height="100%" fill="#868e96"></rect>
+								</image>
+	
+							</div>
+							<div class="col-md-8">
+								<div class="card-body">
+									<h5 class="card-title"> Intern </h5>
+									<ul style="list-style: none;">
+										<li>
+											<p class="card-text">${name}</p>
+								
+							
+											<p class="card-text">${email}</p>
+								
+							
+											<p class="card-text">${id}</p>
+								
+							
+											<p class="card-text">${school}</p>
+										</li>
+	
+									</ul>
+	
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!--card 4 (ME)-->
+				<div class="col">
+					<div class="card">
+						<div class="row g-0">
+							<div class="col-md-4">
+								<image class="bd-placeholder-img" width="100%" height="250"
+									src="assets/man-avatar-pngrepo-com.png" role="img" aria-label="Placeholder: Image"
+									preserveAspectRatio="xMidYMid slice" focusable="false">
+									<title>Placeholder</title>
+									<rect width="100%" height="100%" fill="#868e96"></rect>
+								</image>
+	
+							</div>
+							<div class="col-md-8">
+								<div class="card-body">
+									<h5 class="card-title">Programmer</h5>
+									<ul style="list-style: none;">
+										<li>
+											<p class="card-text">Jason Day</p>
+										</li>
+										<li><a href="https://github.com/shivermotion">
+												<p class="card-text">GitHub</p>
+											</a></li>
+										<li> <a href="https://shivermotion.github.io/jason-day/">
+												<p class="card-text">Portfolio</p>
+											</a></li>
+	
+									</ul>
+	
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	
 	
 	
 	
-		
+	
+	
+	
+	
+	
 	
 	
 		<!-- Option 1: Bootstrap Bundle with Popper -->
